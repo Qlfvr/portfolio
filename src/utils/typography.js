efbbf30ea2db57a8fe36ecb62ba5ac54ce6c0130
Quentin Lefevre <lefevre.quentin@gmail.com>
2020-07-16 11:45:10 +0200

    config typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,4 +1,34 @@
 import Typography from "typography"
-import kirkhamTheme from "typography-theme-kirkham"
-const typography = new Typography(kirkhamTheme)
+// import kirkhamTheme from "typography-theme-kirkham"
+// import altonTheme from "typography-theme-alton"
+// const typography = new Typography(kirkhamTheme)
+// const typography = new Typography(altonTheme)
+
+const typography = new Typography({
+  baseFontSize: "18px",
+  baseLineHeight: 1.666,
+  headerColor : "#333333",
+  bodyColor: "#333333",
+  googleFonts: [
+    {
+      name: "Do Hyeon",
+      styles: [],
+    },
+    {
+      name: "Roboto",
+      styles: []
+    }
+  ],
+
+  headerFontFamily: [
+    "Avenir Next",
+    "Helvetica Neue",
+    "Segoe UI",
+    "Helvetica",
+    "Arial",
+    "sans-serif",
+  ],
+  bodyFontFamily: ["Roboto", "serif"],
+})
+
 export default typography
