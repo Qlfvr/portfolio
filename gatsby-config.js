93af37f9be834cc9f65cfddcf761e7ac056b3f24
Quentin Lefevre <lefevre.quentin@gmail.com>
2020-07-22 16:50:34 +0200

    add gatsby-source-strapi

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,5 +14,20 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+        resolve: `gatsby-source-strapi`,
+        options: {
+          apiURL: `http://localhost:1337`,
+          queryLimit: 1000, // Default to 100
+          contentTypes: [`info`, `technologies`],
+          //If using single types place them in this array.
+        //   singleTypes: [`home-page`, `contact`],
+          // Possibility to login with a strapi user, when content types are not publically available (optional).
+        //   loginData: {
+        //     identifier: "",
+        //     password: "",
+        //   },
+        },
+      },
   ],
 }
